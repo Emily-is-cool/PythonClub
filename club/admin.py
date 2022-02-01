29303f36b8e3a11d72df9875a4fe42af0f631303
Emily-is-cool <79616525+Emily-is-cool@users.noreply.github.com>
2022-02-01 15:47:55 -0800

    new models

--- club/admin.py
+++ club/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import meeting, resource, meetingminute, event
 
 # Register your models here.
+admin.site.register(meeting)
+admin.site.register(resource)
+admin.site.register(meetingminute)
+admin.site.register(event)
--- club/models.py
+++ club/models.py
@@ -1,3 +1,57 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class meeting(models.Model):
+    meetingtitle=models.CharField(max_length=220)
+    meetingdate=models.DateField()
+    meetingtime=models.DateTimeField()
+    meetinglocation=models.CharField(max_length=254)
+    agenda=models.TextField()
+
+    def __str__(self):
+        return self.meetingtitle
+    
+    class Meta:
+        db_table='meeting'
+
+class meetingminute(models.Model):
+    meetingid=models.ForeignKey(meeting, on_delete=models.CASCADE)
+    attendance=models.ManyToManyField(User)
+    minutestext=models.TextField
+
+    def __str__(self):
+        return self.meetingminutes
+    class Meta:
+        db_table='meetingminutes'
+
+class resource(models.Model):
+    resourcename=models.CharField(max_length=254)
+    resourcetype=models.CharField(max_length=254)
+    resourceurl=models.URLField()
+    dateentered=models.DateField()
+    userid=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    resourcedescription=models.TextField()
+
+    def __str__(self):
+        return self.resource
+
+    class Meta:
+        db_table='resource'
+
+class event(models.Model):
+    eventname=models.CharField(max_length=254)
+    eventlocation=models.CharField(max_length=300)
+    eventdate=models.DateField()
+    eventtime=models.DateTimeField()
+    userid=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.eventdate
+
+    class Meta:
+        db_table='event'
+
+
+    
