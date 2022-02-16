82bade82fe82cc1962d8de504e08047ad577bcea
Emily-is-cool <79616525+Emily-is-cool@users.noreply.github.com>
2022-02-16 13:38:59 -0800

    adding meeting details view

--- club/urls.py
+++ club/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('getresource/', views.getresource, name='Resources')
+    path('resources/', views.getresource, name='Resources'),
+    path('meetings/', views.meetingview, name='meetings'),
+    path('meeting_min/<int:id>', views.meeting_min, name='somethingelse'),
 ]
--- club/views.py
+++ club/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import meeting, meetingminute, resource, event
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 def index(request):
@@ -8,3 +10,11 @@ def index(request):
 def getresource(request):
     resource_list=resource.objects.all()
     return render(request, 'club/resources.html', {'resource_list' : resource_list})
+
+def meetingview(request):
+    meeting_list=meeting.objects.all()
+    return render(request, 'club/meetings.html', {'meeting_list' : meeting_list})
+
+def meeting_min(request, id):
+    meet=get_object_or_404(meeting, pk=id)
+    return render(request, 'club/meetingminutes.html', {'meet' : meet})
